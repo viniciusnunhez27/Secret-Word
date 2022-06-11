40e3b1213bf9e7d9a4d8e2755666171ff058a163
vinicius <v.nunhez27@gmail.com>
2022-06-11 15:34:18 -0300

    Screnn game

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -25,18 +25,39 @@ function App() {
   const pickWordAndCategory = () => {
      const categories = Object.keys(words)
      const category = categories [Math.floor(Math.random () * Object.keys(categories).length)];
-     console.log(category)  
+     
     
 
     const word = words[category][Math.floor(Math.random () * words[category].length)]; 
-    console.log(word)
+   
    
     return {word, category};
   }
 
   const startGame = () => { 
-    
+    // pick word and pick category 
+
      const { word,category } = pickWordAndCategory();  
+
+ // create an array of letters
+    
+    let wordLetters = word.split(""); 
+
+    
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log (word,category)
+    console.log(wordLetters)
+
+    //fill states 
+    
+      setPickedWord(word);
+      setPickedCategory(category);
+      setLetters(letters);
+  
+
+    
+    
 
     SetGameStage (stages[1].name)
   }
